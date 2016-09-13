c93d19e919d539b8636b469076c0fd2b3dca025b
Lew Sibal <lew@egissys.com>
2016-09-13 18:22:00 +0800

    Define initialize_internals so you don't have to override initialize itself

--- lib/lackeys/service_base.rb
+++ lib/lackeys/service_base.rb
@@ -1,8 +1,11 @@
 module Lackeys
   class ServiceBase
     def initialize(parent)
+      initialize_internals
       @parent = parent
     end
+
+    def initialize_internals; end
 
     def parent; @parent; end
 
