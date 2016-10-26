d6fc32f027e9bf2acf24aebdc3318040ad03a3ce
Lew Sibal <lew@egissys.com>
2016-10-26 14:02:56 +0800

    Integrate callbacks setup for users of Lackeys::RailsBase

--- spec/lib/lackeys/rails_base_spec.rb
+++ spec/lib/lackeys/rails_base_spec.rb
@@ -1,12 +1,71 @@
 require 'spec_helper'
 require 'lackeys'
+require 'active_model'
 
 describe Lackeys::RailsBase, type: :class do
-  let(:test_class) { Class.new { include Lackeys::RailsBase } }
+  let(:test_class) do
+    Class.new do
+      extend ActiveModel::Callbacks
+      include Lackeys::RailsBase
+    end
+  end
   let(:test_class_instance) { test_class.new }
   let(:registry) { double("Registry Double") }
-  before(:each) do
-    allow(Lackeys::Registry).to receive(:new).once.and_return registry
+  before(:each) do |example|
+    unless RSpec.current_example.metadata[:skip_registry_stub]
+      allow(Lackeys::Registry).to receive(:new).once.and_return registry
+    end
+  end
+
+  describe "should automatically generate callbacks" do
+    let(:test_class_instance) { TestRailsClass.new }
+    let(:before_save_called) { false }
+    let(:after_save_called) { false }
+    let(:before_create_called) { false }
+    let(:after_create_called) { false }
+    class TestRailsClass
+      extend ActiveModel::Callbacks
+      include Lackeys::RailsBase
+    end
+    class TestServiceClass < Lackeys::ServiceBase
+      Lackeys::Registry.register(TestServiceClass, TestRailsClass) do |r|
+        # Do nothing
+        r.add_method :called
+        r.add_callback :before_save, "before_save"
+        r.add_callback :after_save, "after_save"
+        r.add_callback :before_create, "before_create"
+        r.add_callback :after_create, "after_create"
+      end
+
+      def initialize_internals; @called = []; end
+      def called; @called; end
+
+      def before_save; @called << "before_save"; end
+      def after_save; @called << "after_save"; end
+      def before_create; @called << "before_create"; end
+      def after_create; @called << "after_create"; end
+    end
+    subject { test_class_instance.called }
+
+    context "save callbacks" do
+      before(:each) { test_class_instance.run_callbacks(:save) }
+      it "save callbacks", skip_registry_stub: true do
+        should include "before_save"
+        should include "after_save"
+        should_not include "before_create"
+        should_not include "after_create"
+      end
+    end
+
+    context "create callbacks" do
+      before(:each) { test_class_instance.run_callbacks(:create) }
+      it "create callback", skip_registry_stub: true do
+        should_not include "before_save"
+        should_not include "after_save"
+        should include "before_create"
+        should include "after_create"
+      end
+    end
   end
 
   describe "#registry" do
@@ -64,7 +123,10 @@ describe Lackeys::RailsBase, type: :class do
       class ParentDummyClass
         def method_missing(method_name, *args, &block); "200"; end
       end
-      class DummyClass < ParentDummyClass; include Lackeys::RailsBase; end;
+      class DummyClass < ParentDummyClass
+        extend ActiveModel::Callbacks
+        include Lackeys::RailsBase
+      end;
       before(:each) do
         expect(registry).to receive(:method?).with(method_name).and_return false
       end
